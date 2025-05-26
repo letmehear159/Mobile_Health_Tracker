3949755c3b3b2a18cf67e58bec42ecaa31bf59aa
Huy Hoang <hoanghuy140204@gmail.com>
2025-05-26 22:44:36 +0700

    fix_openAIService

--- app/src/main/java/com/example/healthtrackerapp/repository/ChatRepository.java
+++ app/src/main/java/com/example/healthtrackerapp/repository/ChatRepository.java
@@ -15,7 +15,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ChatRepository {
     private static final String BASE_URL = "https://api.openai.com/";
-    private static final String API_KEY = ""; // Replace with your actual API key
+    private static final String API_KEY = "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\n"; // Replace with your actual API key
     private final OpenAIService openAIService;
 
     public ChatRepository() {
